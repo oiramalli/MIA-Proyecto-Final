1cc0bc77ae7cb0ebcbe3c63b03803dd6ff578c89
Mario Alvarado <malvarado@localhost.localdomain>
2017-01-07 10:20:41 -0600

    FIX: No module named local_settings

--- TutoMartinez/settings.py
+++ TutoMartinez/settings.py
@@ -123,7 +123,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'oiramalli@gmail.com'
 EMAIL_HOST_PASSWORD = 'CHANGE_ME'
 EMAIL_PORT = 587
+
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
