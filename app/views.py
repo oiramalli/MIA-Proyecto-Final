c5ea5309c84da965409267681350209fb24e26fe
Mario Alvarado <oiramalli@gmail.com>
2017-01-06 21:21:27 -0600

    Pagina de registro

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 # from .models import
 # Create your views here.
 def index(request):
-    sites_list = [{'href':'#proyect','name':'Proyecto'},{'href':'#about','name':'Acerca de'},{'href':'#contact','name':'Contacto'},{'href':'/login','name':'Ingresar'}]#Question.objects.order_by('-pub_date')[:5]
+    sites_list = [{'href':'#proyect','name':'Proyecto'},{'href':'#about','name':'Acerca de'},{'href':'#contact','name':'Contacto'},{'href':'/login','name':'Ingresar'},{'href':'/register','name':'Registrarse'}]#Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('index.html')
     context = {
         'sites_list': sites_list,
@@ -16,6 +16,16 @@ def login(request):
     sites_list = []#[{'href':'/login','name':'Ingresar'}]#Question.objects.order_by('-pub_date')[:5]
     # sites_list = [{'href':'/#proyect','name':'Proyecto'},{'href':'/#about','name':'Acerca de'},{'href':'/#contact','name':'Contacto'},{'href':'/login','name':'Ingresar'}]#Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('login.html')
+    context = {
+        'sites_list': sites_list,
+        # 'USER':{'NAME':'José Axpuac'},
+    }
+    return HttpResponse(template.render(context, request))
+    # return render_to_response('login.html');
+def register(request):
+    sites_list = []#[{'href':'/login','name':'Ingresar'}]#Question.objects.order_by('-pub_date')[:5]
+    # sites_list = [{'href':'/#proyect','name':'Proyecto'},{'href':'/#about','name':'Acerca de'},{'href':'/#contact','name':'Contacto'},{'href':'/login','name':'Ingresar'}]#Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('register.html')
     context = {
         'sites_list': sites_list,
         # 'USER':{'NAME':'José Axpuac'},
